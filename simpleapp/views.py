3b1e417fc8e37191c07b76225e0357ecaab37e7e
Andr <amyagkov66@mail.ru>
2023-05-12 00:30:40 +0300

    D3.4

---
+++ simpleapp/views.py
@@ -0,0 +1,27 @@
+
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
+from .models import News, Category
+from datetime import datetime
+
+
+class NewsList(ListView):
+    model = News  # указываем модель, объекты которой мы будем выводить
+    template_name = 'news.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'news'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = News.objects.order_by('-id')
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow() # добавим переменную текущей даты time_now
+        context['value1'] = None # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class NewsDetail(DetailView):
+    model = News # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'new.html' # название шаблона будет product.html
+    context_object_name = 'new' # название объекта
+
